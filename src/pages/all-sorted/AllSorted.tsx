e12dae5a80e41422502dd8b627276be1b2e2b240
willco213 <87068102+willco123@users.noreply.github.com>
2023-05-11 14:27:40 +0100

    changed some css

--- src/pages/all-sorted/AllSorted.tsx
+++ src/pages/all-sorted/AllSorted.tsx
@@ -86,39 +86,41 @@ function AllSorted() {
 
   return (
     <div className="all-sorted-wrapper">
-      <div>
-        <button
-          className={sortMode === "breed" ? "glow" : ""}
-          onClick={() => setSortMode("breed")}
-        >
-          Breed
-        </button>
-        <button
-          className={sortMode === "averageRating" ? "glow" : ""}
-          onClick={() => setSortMode("averageRating")}
-        >
-          Rating
-        </button>
-        <button
-          className={sortMode === "numberOfRates" ? "glow" : ""}
-          onClick={() => setSortMode("numberOfRates")}
-        >
-          Votes
-        </button>
-      </div>
-      <div>
-        <button
-          className={sortOrder === "asc" ? "glow" : ""}
-          onClick={() => setSortOrder("asc")}
-        >
-          Ascending
-        </button>
-        <button
-          className={sortOrder === "desc" ? "glow" : ""}
-          onClick={() => setSortOrder("desc")}
-        >
-          Descending
-        </button>
+      <div className="button-container">
+        <div>
+          <button
+            className={sortMode === "breed" ? "glow" : ""}
+            onClick={() => setSortMode("breed")}
+          >
+            Breed
+          </button>
+          <button
+            className={sortMode === "averageRating" ? "glow" : ""}
+            onClick={() => setSortMode("averageRating")}
+          >
+            Rating
+          </button>
+          <button
+            className={sortMode === "numberOfRates" ? "glow" : ""}
+            onClick={() => setSortMode("numberOfRates")}
+          >
+            Votes
+          </button>
+        </div>
+        <div>
+          <button
+            className={sortOrder === "asc" ? "glow" : ""}
+            onClick={() => setSortOrder("asc")}
+          >
+            Ascending
+          </button>
+          <button
+            className={sortOrder === "desc" ? "glow" : ""}
+            onClick={() => setSortOrder("desc")}
+          >
+            Descending
+          </button>
+        </div>
       </div>
 
       <div className="image-data">
